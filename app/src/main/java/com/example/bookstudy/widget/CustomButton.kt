3c06ab50b33531224f9c74547485f5f5c704b3a0
MBKY-766 <fengzhixin@drojian.dev>
2025-08-29 18:19:27 +0800

    自定义控件

---
+++ app/src/main/java/com/example/bookstudy/widget/CustomButton.kt
@@ -0,0 +1,23 @@
+package com.example.bookstudy.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.Button
+import com.example.bookstudy.R
+
+class CustomButton : androidx.appcompat.widget.AppCompatButton {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : this(
+        context,
+        attrs,
+        R.attr.customButtonStyle
+    )
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+}
