4cfaac29c403b4eb8eb97754ad522c09bab7df5b
MBKY-766 <fengzhixin@drojian.dev>
2025-09-04 14:32:50 +0800

    接管手势事件处理，跟踪滑动轨迹实现手写签名

---
+++ bookstudy11/src/main/java/com/example/bookstudy11/bean/PathPosition.java
@@ -0,0 +1,8 @@
+package com.example.bookstudy11.bean;
+
+import android.graphics.PointF;
+
+public class PathPosition {
+    public PointF prePos;//上个落点的横纵坐标
+    public PointF nextPos;//下个落点的横纵坐标
+}
